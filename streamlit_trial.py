a6a6fdb69497247ed19222f0e78d4bbb23617886
Parnika798 <parnikajainjp@gmail.com>
2024-11-08 20:09:15 +0530

    Update streamlit_trial.py

--- streamlit_trial.py
+++ streamlit_trial.py
@@ -103,13 +103,13 @@ with col2:
     # Adding a button to show the summary for the graph
     if st.button('Click to view summary for Ad Spend vs Total Votes'):
             st.write("There is a wide distribution of voter turnout percentages, with values ranging from 40% to 90% across varying levels of ad spend.
-While some data points indicate higher turnout percentages with increased ad spending, the overall correlation appears weak, suggesting that ad expenditure does not consistently lead to higher voter participation.
-The cluster of points around the 60-80% turnout range indicates that many constituencies achieve similar turnout levels despite differing advertising budgets.
-Key Insights
-Weak Correlation: The scatter plot highlights a weak correlation between ad spending and voter turnout, suggesting that increased advertising does not guarantee higher participation.
-Diverse Turnout Levels: Voter turnout percentages show significant variation, indicating that many factors influence engagement beyond just ad spend.
-Potential Influencers: Constituencies with low ad spend but high turnout may be influenced by other variables such as local issues, candidate appeal, or grassroots campaigning efforts.
-Strategic Adjustments: Political parties may need to reassess their reliance on advertising as a primary tool for boosting voter turnout, exploring alternative strategies for engaging voters.
+                      While some data points indicate higher turnout percentages with increased ad spending, the overall correlation appears weak, suggesting that ad expenditure does not consistently lead to higher voter participation.
+                      The cluster of points around the 60-80% turnout range indicates that many constituencies achieve similar turnout levels despite differing advertising budgets.
+                      Key Insights
+                      Weak Correlation: The scatter plot highlights a weak correlation between ad spending and voter turnout, suggesting that increased advertising does not guarantee higher participation.
+                      Diverse Turnout Levels: Voter turnout percentages show significant variation, indicating that many factors influence engagement beyond just ad spend.
+                      Potential Influencers: Constituencies with low ad spend but high turnout may be influenced by other variables such as local issues, candidate appeal, or grassroots campaigning efforts.
+                      Strategic Adjustments: Political parties may need to reassess their reliance on advertising as a primary tool for boosting voter turnout, exploring alternative strategies for engaging voters.
 
 ")
 
