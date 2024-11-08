19094e86a725133e7459348ed83ac32e855fce65
Parnika798 <parnikajainjp@gmail.com>
2024-11-08 21:38:25 +0530

    Update streamlit_trial.py

--- streamlit_trial.py
+++ streamlit_trial.py
@@ -147,9 +147,13 @@ with col8:
 # Assuming 'Amount spent (INR)' is the column for ad spend in INR
    total_amount_spent = advtdf['Amount spent (INR)'].sum()
 
-# Displaying the KPI with a clear and large font
-   st.markdown("<h3 style='text-align: center; color: Blue;'>Total Amount Spent on Advertising (INR)</h3>", unsafe_allow_html=True)
-   st.markdown(f"<h1 style='text-align: center; color: #4CAF50;'>₹{total_amount_spent:,.0f}</h1>", unsafe_allow_html=True)
+   st.markdown("""
+    <div style="border: 5px solid black; padding: 20px; border-radius: 10px; text-align: center;">
+        <h3 style="color: Dark Blue;">Total Amount Spent on Advertising (INR)</h3>
+        <h1 style="color: #4CAF50;">₹{total_amount_spent:,.0f}</h1>
+    </div>
+   """, unsafe_allow_html=True)
+
 
 
 col5, col6= st.columns([6,6])
