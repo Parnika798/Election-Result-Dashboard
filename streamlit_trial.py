a7f58968f7a289bce894778151359e89fcd1c6d4
Parnika798 <parnikajainjp@gmail.com>
2024-11-08 10:30:06 +0530

    Update streamlit_trial.py

--- streamlit_trial.py
+++ streamlit_trial.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1mmtSSZQeV9PDgqJ1t9u7J1mmKbmurlZq
 """
 
-!pip install streamlit
+
 !pip install pyngrok
 
 # Commented out IPython magic to ensure Python compatibility.
